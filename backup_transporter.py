48eb7293720de6fbbdc43a5ab299820ee1a065d2
a.a.galeev <archius11@gmail.com>
2024-12-28 11:34:19 +0300

    added try-except to file copy

--- backup_transporter.py
+++ backup_transporter.py
@@ -25,12 +25,19 @@ def transport_daily_backup(backup_date: date, only_last_backup):
 
         ftp = FTPOperator(FTP_WRITER)
         for file_name in files_list:
-            copy_to_ftp(ftp, folders['ftp'], file_name)
-            print(f'copied {file_name} to ftp')
-            copy_to_storage(folders['storage'], file_name)
-            print(f'copied {file_name} to storage')
-    if not only_last_backup:
-        announcer.announce_successful('Перенос бэкапов в хранилище и в FTP успешно завершен')
+            try:
+                copy_to_ftp(ftp, folders['ftp'], file_name)
+                print(f'copied {file_name} to ftp')
+            except:
+                pass
+
+            try:
+                copy_to_storage(folders['storage'], file_name)
+            except:
+                pass
+                print(f'copied {file_name} to storage')
+
+    announcer.announce_successful('Перенос бэкапов в хранилище и в FTP завершен')
 
 
 def copy_to_ftp(ftp, folder_ftp, file_name):
@@ -59,15 +66,26 @@ def copy_to_ftp(ftp, folder_ftp, file_name):
 
 def copy_to_storage(folder_storage, file_name):
     storage_filename = str(os.path.join(BACKUP_STORAGE, folder_storage, os.path.basename(file_name)))
-    try:
-        shutil.copy2(file_name, storage_filename)
-    except FileNotFoundError as e:
-        if not os.path.exists(file_name):
-            announcer.announce_error('Путь не найден: ' + file_name)
-        if not os.path.exists(storage_filename):
-            announcer.announce_error('Путь не найден ' + storage_filename)
-        announcer.announce_error(str(e))
-    except Exception as e:
-        announcer.announce_error('Неизвестная ошибка при записи в хранилище: ' + file_name)
-        announcer.announce_error(str(e))
-        raise e
+    last_exception = None
+
+    for attempt in range(0, 5):
+
+        try:
+             shutil.copy2(file_name, storage_filename)
+        except FileNotFoundError as e:
+            if not os.path.exists(file_name):
+                announcer.announce_error('Путь не найден: ' + file_name)
+            if not os.path.exists(storage_filename):
+                announcer.announce_error('Путь не найден ' + storage_filename)
+            announcer.announce_error(str(e))
+            last_exception = e
+
+        except Exception as e:
+            announcer.announce_error('Неизвестная ошибка при записи в хранилище: ' + file_name)
+            announcer.announce_error(str(e))
+            last_exception = e
+
+    if last_exception:
+        announcer.announce_error('Запись в хранилище не удалась: ' + file_name)
+        announcer.announce_error(str(last_exception))
+        raise last_exception
